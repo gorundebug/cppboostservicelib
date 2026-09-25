ee7ee02b9d7f6c2f2a2bba79b7255478c4591a3a
Sergey Alexeev <sergeyalexeev@yahoo.com>
2026-09-25 16:52:39 +0300

    Require explicit gRPC tracing mode in Boost calls

--- include/servicelib/datasink/grpc/common.hpp
+++ include/servicelib/datasink/grpc/common.hpp
@@ -187,7 +187,7 @@ struct CallOptions final {
 };
 
 inline CallOptions callOptions(const MessageContext& context,
-                               bool tracingEnabled = true) {
+                               bool tracingEnabled) {
   return CallOptions{context, tracingEnabled};
 }
 
